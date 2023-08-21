4c3225bfc8b68b083d74be958692ae33b6b519ca
GourabSanyal <sanyal.gourab@gmail.com>
2023-08-21 17:06:30 +0530

    derver db index added schema-model

---
+++ vite-course-selling-app/server/db/index.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+const mongoose = require("mongoose");
+
+// mongoose schema
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  purchasedCourses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Course" }],
+});
+
+const adminSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const courseSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  prices: Number,
+  imageLink: String,
+  published: Boolean,
+});
+
+// mongoose collection
+
+const User = mongoose.model("User", userSchema);
+const Admin = mongoose.model("Admin", adminSchema);
+const Course = mongoose.model("Course", courseSchema);
+
+module.exports = {
+  User,
+  Admin,
+  Course,
+};
