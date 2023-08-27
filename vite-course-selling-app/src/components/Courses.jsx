25eb0d715b824b76e4047de2feabddb7f013a03d
GourabSanyal <sanyal.gourab@gmail.com>
2023-08-27 11:43:04 +0530

    courses logic updated to axios, UI changed

--- vite-course-selling-app/src/components/Courses.jsx
+++ vite-course-selling-app/src/components/Courses.jsx
@@ -1,51 +1,80 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
-import { Card, Typography } from "@mui/material";
+import { Card, Typography, Button } from "@mui/material";
+import axios from "axios";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Courses() {
   const [courses, setCourses] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:3000/admin/courses", {
-      method: "GET",
+  const getdata = async () => {
+    const data = await axios.get("http://localhost:3000/admin/courses", {
       headers: {
         Authorization: "Bearer " + localStorage.getItem("token"),
       },
-    })
-      .then((res) => res.json())
-      .then((data) => setCourses(data.courses));
-  });
+    });
+    setCourses(data.data.courses);
+    // console.log(data.data.courses)
+    // console.log(courses)
+  };
+
+  useEffect(() => {
+    getdata();
+
+    // fetch("http://localhost:3000/admin/courses", {
+    //   method: "GET",
+    //   headers: {
+    //     Authorization: "Bearer " + localStorage.getItem("token"),
+    //   },
+    // })
+    //   .then((res) => res.json())
+    //   .then((data) => setCourses(data.courses));
+  }, []);
+
+  if (!courses) {
+    return <>Loading...</>;
+  }
+
   return (
-    <div style={{ display: 'flex', flexWrap: "wrap", justifyContent: 'center'}}>
+    <div
+      style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+    >
       {courses.map((course) => {
-        return <Course course={course} />;
+        return <Course key={course._id} course={course} />;
       })}
     </div>
   );
 }
 
-export function Course(props) {
+export function Course({ course }) {
+  const navigate = useNavigate();
   return (
     <Card
       style={{
-        border: "2px solid black",
         margin: 10,
         width: 300,
         minHeight: 200,
+        padding: 20,
       }}
     >
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "column",
-        }}
-      >
-        <Typography variant="h5">{props.course.title}</Typography>
-        <Typography variant="subtitle">{props.course.description}</Typography>
-        <img src={props.course.imageLink} style={{ width : '100%', height: '100%'}}/>
+      <Typography textAlign={"center"} variant="h5">
+        {course.title}
+      </Typography>
+      <Typography textAlign={"center"} variant="subtitle1">
+        {course.description}
+      </Typography>
+      <img src={course.imageLink} style={{ width: 300 }}></img>
+      <div style={{ display: "flex", justifyContent: "center", marginTop: 20 }}>
+        <Button
+          variant="contained"
+          size="large"
+          onClick={() => {
+            navigate("/course/" + course._id);
+          }}
+        >
+          Edit
+        </Button>
       </div>
     </Card>
   );
