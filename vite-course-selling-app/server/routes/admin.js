998769ec655976f62dae016bc316f1c30a6edb27
GourabSanyal <sanyal.gourab@gmail.com>
2023-08-28 10:08:51 +0530

    minor changes - eslint, packages

--- vite-course-selling-app/server/routes/admin.js
+++ vite-course-selling-app/server/routes/admin.js
@@ -5,15 +5,15 @@ const express = require("express");
 const { Course, Admin } = require("../db/index");
 const jwt = require("jsonwebtoken");
 const { SECRET } = require("../middleware/auth");
-const {authenticateJwt} = require("../middleware/auth");
+const { authenticateJwt } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.get("/me", authenticateJwt, async (req, res) => {
   const admin = await Admin.findOne({ username: req.user.username });
-  if(!admin){
-    res.status(403).json({ message: "Admin dosent exist"})
-    return 
+  if (!admin) {
+    res.status(403).json({ message: "Admin dosent exist" });
+    return;
   }
   res.json({
     username: admin.username,
@@ -54,30 +54,32 @@ router.post("/login", async (req, res) => {
 router.post("/courses", authenticateJwt, async (req, res) => {
   const course = new Course(req.body);
   await course.save();
-  res.json({ message: "Course added succesfully", courseId : course.id });
+  res.json({ message: "Course added succesfully", courseId: course.id });
 });
 
 //admin update a course
-router.put('/courses/:courseId', authenticateJwt, async(req, res) => {
-    const course = await Course.findByIdAndUpdate(req.params.courseId, req.body, { new:  true})
-    if(!course){
-        res.status(400).json({ message: "Course is not found"})
-    } else {
-        res.json({ message: "Course updated successfully"})
-    }
-})
+router.put("/courses/:courseId", authenticateJwt, async (req, res) => {
+  const course = await Course.findByIdAndUpdate(req.params.courseId, req.body, {
+    new: true,
+  });
+  if (!course) {
+    res.status(400).json({ message: "Course is not found" });
+  } else {
+    res.json({ message: "Course updated successfully" });
+  }
+});
 
 // admin get all courses
 
-router.get('/courses', authenticateJwt, async(req, res) => {
-    const courses = await Course.find({});
-    res.json({ courses })
-})
+router.get("/courses", authenticateJwt, async (req, res) => {
+  const courses = await Course.find({});
+  res.json({ courses });
+});
 
-router.get('/course/:courseId', authenticateJwt, async(req, res) => {
+router.get("/course/:courseId", authenticateJwt, async (req, res) => {
   const courseId = req.params.courseId;
   const course = await Course.findById(courseId);
-  res.json({ course })
-})
+  res.json({ course });
+});
 
 module.exports = router;
