6a32c011b3811aacac2f316493dc33cafa5e6a0a
GourabSanyal <sanyal.gourab@gmail.com>
2023-08-28 10:08:32 +0530

    course schema error fix

--- vite-course-selling-app/server/db/index.js
+++ vite-course-selling-app/server/db/index.js
@@ -17,7 +17,7 @@ const adminSchema = new mongoose.Schema({
 const courseSchema = new mongoose.Schema({
   title: String,
   description: String,
-  prices: Number,
+  price: Number,
   imageLink: String,
   published: Boolean,
 });
