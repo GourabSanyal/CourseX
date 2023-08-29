c202f17e03211b21c3a368d050cd0f569406b69f
GourabSanyal <sanyal.gourab@gmail.com>
2023-08-30 00:41:48 +0530

    ts - week 7 ts

--- week-7-typescript/index.js
+++ week-7-typescript/index.js
@@ -1,5 +1,4 @@
 // run `tsc index.js` to compile it to a js file
-
 // function calculateSum1(
 //     a: number, 
 //     b: number, 
@@ -18,54 +17,39 @@
 //     }
 //     return -1
 //   }
-  
 //   const answer = calculateSum1(2,2,'div')
 //   console.log(answer);
-
-
 //types lets you do Unions or Ors
-
 // interface Circle {
 //     radius : number
 // }
-
 // interface Square {
 //     side: number
 // }
-
 // interface Reactangle {
 //     width : number,
 //     height: number
 // }
-
 // type Shape = Reactangle | Square | Circle ; // this is Or - '|' and this is And '&'
-
 // function renderShape(shape : Shape){
 //     console.log("Rendered");
 // }
-
 // function calculateArea (shape: Shape){
 //     console.log("Calculated area");
-    
 // }
-
 // renderShape({
 //     radius : 10
 // })
-
-enum Arithmetic {
-    Add,
-    Sub,
-    Mul,
-    Div
+var Arithmetic;
+(function (Arithmetic) {
+    Arithmetic[Arithmetic["Add"] = 0] = "Add";
+    Arithmetic[Arithmetic["Sub"] = 1] = "Sub";
+    Arithmetic[Arithmetic["Mul"] = 2] = "Mul";
+    Arithmetic[Arithmetic["Div"] = 3] = "Div";
+})(Arithmetic || (Arithmetic = {}));
+function calculate(a, b, type) {
+    console.log(type);
+    return 1;
 }
-
- function calculate( a: number, b:number, type :Arithmetic) {
-     console.log(type);
-     return 1
-     
- }
-
- const ans = calculate(1,2, Arithmetic.Div)
- console.log();
-   
+var ans = calculate(1, 2, Arithmetic.Div);
+console.log();
