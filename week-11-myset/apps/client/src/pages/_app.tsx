1ae2b9a9e07e03135cecb1755a4f49697f971bf6
GourabSanyal <sanyal.gourab@gmail.com>
2023-10-09 10:53:53 +0530

    BE - me rout updated

--- week-11-myset/apps/client/src/pages/_app.tsx
+++ week-11-myset/apps/client/src/pages/_app.tsx
@@ -1,58 +1,68 @@
 import "@/styles/globals.css";
 import axios from "axios";
 import type { AppProps } from "next/app";
-import { useRecoilState, useRecoilValue } from "recoil";
+import { RecoilRoot, useRecoilState, useRecoilValue } from "recoil";
 import { isUserLoading, userState } from "store";
-import { useEffect} from 'react'
+import { useEffect } from "react";
+import Appbar from "@/components/AppBar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <App2 Component={Component} pageProps={...pageProps} />;
+    </RecoilRoot>
+  );
 }
 
-function App2({ Component, pageProps}) {
-  const userLoading = useRecoilValue(isUserLoading)
-  if (isUserLoading){
-    return <div>
-      Loading...
-      <InitUser />
-    </div>
+function App2({ Component, pageProps }) {
+  const userLoading = useRecoilValue(isUserLoading);
+  if (userLoading) {
+    return (
+      <div>
+        Loading...
+        <InitUser />
+      </div>
+    );
   }
-  return <AppBar />
+  return (
+    <div>
+      <Appbar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-function InitUser(){
+function InitUser() {
   const setUser = useRecoilState(userState);
   const init = async () => {
     try {
       const response = await axios.get(`/api/auth/me`, {
-        headers : {
-          "Authorization" : "Barear " + localStorage.getItem("token")
-        }
-    
-      });
+          headers: {
+              "Authorization": "Bearer " + localStorage.getItem("token")
+          }
+      })
 
       if (response.data.user) {
-        setUser({
-          isLoading: false,
-          userEmail : response.data.user.username
-        })
+          setUser({
+              isLoading: false,
+              userEmail: response.data.user.username
+          })
       } else {
-        setUser({
-          isLoading :  false,
-          userEmail : null
-        })
+          setUser({
+              isLoading: false,
+              userEmail: null
+          })
       }
-      
-    } catch (error) {
+  } catch (e) {
+
       setUser({
-        isLoading: false,
-        userEmail : null
+          isLoading: false,
+          userEmail: null
       })
-    }
+  }
   };
   useEffect(() => {
-    InitUser()
-  }, [])
-  return <></>
-};
-
+    init();
+  }, []);
+  return <></>;
+}
