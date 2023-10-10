d1284c6d2d07cca523fdfeabd825ac4dc6084d79
GourabSanyal <sanyal.gourab@gmail.com>
2023-10-10 19:00:33 +0530

    route - course route update

---
+++ week-11-myset/apps/client/src/pages/courses.tsx
@@ -0,0 +1,14 @@
+import { Signup } from "ui";
+import axios from "axios";
+import { userState } from "store";
+import {useRouter} from 'next/router';
+import { useSetRecoilState } from "recoil";
+
+export default function SignPage() {
+  
+  return (
+    <div>
+      Course route
+    </div>
+  );
+}
