a8ab3b2b3dac84f51c2d3f7f341a0e716cff6831
GourabSanyal <sanyal.gourab@gmail.com>
2023-10-17 18:52:29 +0530

    ui - route signup logic update

--- week-11-myset/apps/client/src/pages/signup.tsx
+++ week-11-myset/apps/client/src/pages/signup.tsx
@@ -1,14 +1,28 @@
 import { Signup } from "ui";
 import axios from "axios";
+import { userState } from "store";
+import { useRouter } from "next/navigation";
+import {useEffect} from "react";
+import { useSetRecoilState } from "recoil";
 
-export default function SignPage () {
-    return <div>
-        <Signup onClick={async( username, password) => {
-           const response =  await axios.post( "api/signup", {
-            username,
-            password
-           });
-           localStorage.setItem("token",response.data.token)
-        }}/>
+export default function SignPage() {
+
+  const router = useRouter();
+
+  return (
+    <div>
+      <Signup
+        onClick={async (email, password) => {
+          const response = await axios.post("/api/auth/signup", {
+            email,
+            password,
+          });
+          console.log("from page -> ", { email, password });
+          localStorage.setItem("token", response.data.token);
+          router.refresh();
+          router.push("/courses");
+        }}
+      />
     </div>
+  );
 }
