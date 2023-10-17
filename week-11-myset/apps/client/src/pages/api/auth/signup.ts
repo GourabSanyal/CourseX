5205b63b9f804f0c253b7a9da480dc594420971d
GourabSanyal <sanyal.gourab@gmail.com>
2023-10-17 18:53:27 +0530

    be - signup api logic added

--- week-11-myset/apps/client/src/pages/api/auth/signup.ts
+++ week-11-myset/apps/client/src/pages/api/auth/signup.ts
@@ -2,9 +2,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ensureDbConnected } from "@/lib/dbConnect";
 import { verifyTokenAndGetUser } from "@/lib/verifyTokenAndGetUser";
+import { Admin } from "db";
+import jwt from "jsonwebtoken";
 
 type Data = {
-  name: string;
+ email:string,
+ password: string
 };
 
 export default async function handler(
@@ -12,6 +15,21 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   await ensureDbConnected();
- 
+  const { email, password } = req.body;
+  let username = email;
+  console.log("username - password ->", username, password);
   
+  let admin = await Admin.findOne({ username })
+  console.log('from route -->', req.body);
+  
+  if (admin){
+    res.status(403).json({ message : "Admin already exist"})
+  }
+  else {
+    const obj = { username : username , password : password}
+    const newAdmin = new Admin(obj)
+    newAdmin.save()
+    const token = jwt.sign({ username, role : 'admin' }, "SECRET", { expiresIn : '3h'})
+    res.json({ message : "Admin created successfully", token})
+  }
 }
