60586aabe5cd41d110f2e8754f50f574f02d203d
GourabSanyal <sanyal.gourab@gmail.com>
2023-10-17 18:53:48 +0530

    ui - me route update

--- week-11-myset/apps/client/src/pages/api/auth/me.ts
+++ week-11-myset/apps/client/src/pages/api/auth/me.ts
@@ -15,12 +15,12 @@ export default async function handler(
   const authHeader = req.headers.authorization;
   if (authHeader) {
     const token = authHeader.split(" ")[1];
-    verifyTokenAndGetUser(token, (userId: string) => {
-      if (!userId) {
+    verifyTokenAndGetUser(token, (user: string) => {
+      if (!user) {
         res.status(403).json({});
-        return;
+        return
       }
-      res.json({ user: userId });
+      res.json({ user: user });
     });
   }
 }
