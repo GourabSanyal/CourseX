3921f52fb1581d07bfc843a271ee0552ac35bc10
GourabSanyal <sanyal.gourab@gmail.com>
2023-10-31 00:31:46 +0530

    feat : all courses rendered after signup

--- week-11-myset/apps/client/src/pages/courses.tsx
+++ week-11-myset/apps/client/src/pages/courses.tsx
@@ -1,9 +1,21 @@
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import ForcedReload from "@/components/ForceReload";
+import axios from "axios";
+import { Card, Typography, Button } from "@mui/material";
+// import { Button } from "ui";
+
+type Course = {
+  _id: string; // You can adjust this type as needed
+  title: string;
+  description: string;
+  price: number;
+  imageLink: string;
+  published: boolean;
+};
 
 export default function CoursesPage() {
   const router = useRouter();
+  const [courses, setCourses] = useState([])
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -14,10 +26,68 @@ export default function CoursesPage() {
     }
   }, []);
 
-  return (
+  const getData = async() => {
+    const data = await axios.get("/api/auth/courses", {
+      headers : { Authorization : "Bearer " + localStorage.getItem("token")}
+    })
+    setCourses(data.data)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  if (!courses.length){
     <div>
-      {/* <ForcedReload /> */}
-      <div>Courses route</div>
+      Loading Courses ...
     </div>
+  }
+
+  return (
+    <div
+    style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
+  >
+    heyy 
+    {courses.map((course: Course[]) => {
+      return <Course 
+        key={course._id} 
+        course={course} />;
+    })}
+  </div>
+  );
+  }
+
+function Course({ course }) {
+  // const navigate = useNavigate();
+  const router = useRouter();
+  return (
+    <Card
+      style={{
+        margin: 10,
+        width: 300,
+        minHeight: 200,
+        padding: 20,
+      }}
+    >
+      <Typography textAlign={"center"} variant="h5">
+        {course.title}
+      </Typography>
+      <Typography textAlign={"center"} variant="subtitle1">
+        {course.description}
+      </Typography>
+      <img src={course.imageLink} style={{ width: 300 }}></img>
+      <div style={{ display: "flex", justifyContent: "center", marginTop: 20 }}>
+        <Button
+          variant="contained"
+          size="large"
+          // onClick={() => {
+          //   navigate("/course/" + course._id);
+          // }}
+        >
+          Edit
+        </Button>
+      </div>
+    </Card>
   );
 }
+
