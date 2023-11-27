b32f5d7c4d86f214051444cca6226955eb1e4540
GourabSanyal <sanyal.gourab@gmail.com>
2023-11-27 18:01:37 +0530

    UI - grey topper added

---
+++ week-11-myset/apps/client/src/components/ui/GrayTopper.tsx
@@ -0,0 +1,39 @@
+import { Typography } from "@mui/material";
+
+type TitleType = {
+  title: string;
+};
+
+export default function GrayTopper({ title }: TitleType) {
+  return (
+    <div
+      style={{
+        height: 250,
+        background: "#212121",
+        top: 0,
+        width: "100vw",
+        zIndex: 0,
+        marginBottom: -250,
+      }}
+    >
+      <div
+        style={{
+          height: 250,
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div>
+          <Typography
+            style={{ color: "white", fontWeight: 600 }}
+            variant="h3"
+            textAlign={"center"}
+          >
+            {title}
+          </Typography>
+        </div>
+      </div>
+    </div>
+  );
+}
