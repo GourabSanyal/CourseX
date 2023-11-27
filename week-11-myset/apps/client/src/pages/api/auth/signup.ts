9172c91e6c24260af4a944d460f69a329660f541
GourabSanyal <sanyal.gourab@gmail.com>
2023-11-27 18:02:39 +0530

    db - signup route finalised

--- week-11-myset/apps/client/src/pages/api/auth/signup.ts
+++ week-11-myset/apps/client/src/pages/api/auth/signup.ts
@@ -6,8 +6,8 @@ import { Admin } from "db";
 import jwt from "jsonwebtoken";
 
 type Data = {
- email:string,
- password: string
+  email: string;
+  password: string;
 };
 
 export default async function handler(
@@ -17,19 +17,18 @@ export default async function handler(
   await ensureDbConnected();
   const { email, password } = req.body;
   let username = email;
-  console.log("username - password ->", username, password);
-  
-  let admin = await Admin.findOne({ username })
-  console.log('from route -->', req.body);
-  
-  if (admin){
-    res.status(403).json({ message : "Admin already exist"})
-  }
-  else {
-    const obj = { username : username , password : password}
-    const newAdmin = new Admin(obj)
-    newAdmin.save()
-    const token = jwt.sign({ username, role : 'admin' }, "SECRET", { expiresIn : '3h'})
-    res.json({ message : "Admin created successfully", token})
+
+  let admin = await Admin.findOne({ username });
+
+  if (admin) {
+    res.status(403).json({ message: "Admin already exist" });
+  } else {
+    const obj = { username: username, password: password };
+    const newAdmin = new Admin(obj);
+    newAdmin.save();
+    const token = jwt.sign({ username, role: "admin" }, "SECRET", {
+      expiresIn: "3h",
+    });
+    res.json({ message: "Admin created successfully", token });
   }
 }
