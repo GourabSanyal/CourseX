29d9083b34ca92f72dd8567cd677e82277caba32
GourabSanyal <sanyal.gourab@gmail.com>
2023-11-30 19:39:42 +0530

    feat - be - user login api created

---
+++ week-11-myset/apps/client/src/pages/api/auth/user/signup.ts
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ensureDbConnected } from "@/lib/dbConnect";
+import { verifyTokenAndGetUser } from "@/lib/verifyTokenAndGetUser";
+import { User } from "db";
+import jwt from "jsonwebtoken";
+
+type Data = {
+  email: string;
+  password: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  await ensureDbConnected();
+  const { username, password } = req.headers;
+
+  let user = await User.findOne({ username });
+
+  if (user) {
+    res.status(403).json({ message: "User already exist" });
+  } else {
+    const obj = { username: username, password: password };
+    const newUser = new User(obj);
+    newUser.save();
+    const token = jwt.sign({ username, role: "admin" }, "SECRET", {
+      expiresIn: "3h",
+    });
+    res.json({ message: "User registered successfully", token });
+  }
+}
