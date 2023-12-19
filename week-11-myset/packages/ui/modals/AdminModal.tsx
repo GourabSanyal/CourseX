2ed6c3e8be5a4f8b62159992ff0d4ffc34b55167
GourabSanyal <sanyal.gourab@gmail.com>
2023-12-19 16:19:29 +0530

    ui - admin login modal ts error fix

---
+++ week-11-myset/packages/ui/modals/AdminModal.tsx
@@ -0,0 +1,77 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable react/jsx-sort-props */
+// components/AdminModal.tsx
+import React, { useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Typography,
+  TextField,
+  Button,
+  Link,
+} from "@mui/material";
+
+interface AdminModalProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+export function AdminModal({ open, onClose }: AdminModalProps) : React.JSX.Element {
+  const [isSignIn, setIsSignIn] = useState(true);
+
+  const handleToggleForm = () => {
+    setIsSignIn(!isSignIn);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Implement your login or signup logic here
+  };
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>
+        {isSignIn ? "Admin Sign In" : "Admin Sign Up"}
+        <Button
+          onClick={onClose}
+          style={{ position: "absolute", top: 10, right: 10 }}
+        >
+          X
+        </Button>
+      </DialogTitle>
+      <DialogContent>
+        <form onSubmit={handleSubmit}>
+          <TextField label="Email" fullWidth margin="normal" required />
+          <TextField
+            label="Password"
+            type="password"
+            fullWidth
+            margin="normal"
+            required
+          />
+          {!isSignIn && (
+            <TextField
+              label="Confirm Password"
+              type="password"
+              fullWidth
+              margin="normal"
+              required
+            />
+          )}
+          <Button type="submit" variant="contained" color="primary" fullWidth>
+            {isSignIn ? "Sign In" : "Sign Up"}
+          </Button>
+        </form>
+        <Typography variant="body2" align="center" style={{ marginTop: 10 }}>
+          {isSignIn
+            ? "Don't have an admin account? "
+            : "Already have an admin account? "}
+          <Link component="button" variant="body2" onClick={handleToggleForm}>
+            {isSignIn ? "Register as an Admin here" : "Sign In here"}
+          </Link>
+        </Typography>
+      </DialogContent>
+    </Dialog>
+  );
+};
