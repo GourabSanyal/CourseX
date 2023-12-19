1fab5891abf2ed6b7d4288edea0170e0bbdb29d8
GourabSanyal <sanyal.gourab@gmail.com>
2023-12-19 16:19:52 +0530

    ui - appbar redirection admin login fixed

--- week-11-myset/apps/client/src/components/AppBar.tsx
+++ week-11-myset/apps/client/src/components/AppBar.tsx
@@ -1,70 +1,82 @@
 import { Button, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-import { useRecoilValue,  useSetRecoilState } from "recoil";
+import { useRecoilValue, useSetRecoilState } from "recoil";
 import { isUserLoading, userEmailState, userState } from "store";
-import {useRouter} from 'next/navigation';
+import { useRouter } from "next/navigation";
+import { AdminModal } from "ui";
 
 function Appbar() {
   const userLoading = useRecoilValue(isUserLoading);
   const userEmail = useRecoilValue(userEmailState);
   const setUser = useSetRecoilState(userState);
   const router = useRouter();
-  
-  if (userLoading){ 
-    return <></>
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openAdminLoginModal = () => {
+    setModalOpen(true);
+  };
+  const closeAdminLoginModal = () => {
+    setModalOpen(false);
+  };
+
+  if (userLoading) {
+    return <></>;
   }
 
-  if (userEmail) {  
-    return <div style={{
-      display: "flex",
-      justifyContent: "space-between",
-      padding: 4,
-      zIndex: 1
-  }}>
-      <div style={{
-        marginLeft: 10, 
-        cursor: "pointer",
-      }} onClick={() => 
-          router.push('/')
-      }
+  if (userEmail) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: 4,
+          zIndex: 1,
+        }}
       >
+        <div
+          style={{
+            marginLeft: 10,
+            cursor: "pointer",
+          }}
+          onClick={() => router.push("/")}
+        >
           <Typography variant={"h6"}>Coursera</Typography>
-      </div>
+        </div>
 
-      <div style={{display: "flex"}}>
-          <div style={{marginRight: 10, display: "flex"}}>
-          <div style={{marginRight: 10}}>
-                  <Button
-                      onClick={() => {
-                          
-                      }}
-                  >Add course</Button>
-              </div>
+        <div style={{ display: "flex" }}>
+          <div style={{ marginRight: 10, display: "flex" }}>
+            <div style={{ marginRight: 10 }}>
+              <Button onClick={() => {}}>Add course</Button>
+            </div>
 
-              <div style={{marginRight: 10}}>
-                  <Button
-                      onClick={() => {
-                          router.push('/courses')
-                      }}
-                  >Courses</Button>
-              </div>
-
+            <div style={{ marginRight: 10 }}>
               <Button
-                  variant={"contained"}
-                  onClick={() => {
-                      localStorage.removeItem("token");
-                      localStorage.removeItem("pageRefreshed")
-                      setUser({
-                          isLoading: false,
-                          userEmail: null
-                      })
-                      router.push('/')
-                  }}
-              >Logout</Button>
-          </div>
-      </div>
-  </div>
+                onClick={() => {
+                  router.push("/courses");
+                }}
+              >
+                Courses
+              </Button>
+            </div>
 
+            <Button
+              variant={"contained"}
+              onClick={() => {
+                localStorage.removeItem("token");
+                localStorage.removeItem("pageRefreshed");
+                setUser({
+                  isLoading: false,
+                  userEmail: null,
+                });
+                router.push("/");
+              }}
+            >
+              Logout
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
   } else {
     return (
       <div
@@ -74,25 +86,26 @@ function Appbar() {
           paddingTop: 4,
         }}
       >
-        <div style={{ cursor : "pointer"}} onClick={() => router.push('/')}>
+        <div style={{ cursor: "pointer" }} onClick={() => router.push("/")}>
           <Typography variant={"h6"}>Coursera</Typography>
         </div>
         <div style={{ display: "flex" }}>
-        <div style={{ marginRight: 10 }}>
+          <div style={{ marginRight: 10 }}>
             <Button
               // variant={"contained"}
               onClick={() => {
-                // navigate("/signin");
+                openAdminLoginModal();
               }}
             >
               Admin Login
             </Button>
+            <AdminModal open={modalOpen} onClose={closeAdminLoginModal} />
           </div>
           <div style={{ marginRight: 10 }}>
             <Button
               variant={"contained"}
               onClick={() => {
-                router.push("/user/signin")
+                router.push("/user/signin");
               }}
             >
               Sign In
