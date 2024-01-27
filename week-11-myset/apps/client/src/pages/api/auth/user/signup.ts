3463187dea361566e0ac05fa9f7ff7d15f386276
GourabSanyal <sanyal.gourab@gmail.com>
2024-01-27 21:50:26 +0530

    api - user signup api username input and error handling

--- week-11-myset/apps/client/src/pages/api/auth/user/signup.ts
+++ week-11-myset/apps/client/src/pages/api/auth/user/signup.ts
@@ -6,6 +6,7 @@ import { User } from "db";
 import jwt from "jsonwebtoken";
 
 type Data = {
+  username: string;
   email: string;
   password: string;
 };
@@ -20,18 +21,29 @@ export default async function handler(
   res: NextApiResponse<Data | responseData>
 ) {
   await ensureDbConnected();
-  const { email, password } = req.body;
-  let username = email;
+  const { username, email, password } = req.body;
+  let user = await User.findOne({ email });
 
-  let user = await User.findOne({ username });
+  if (!username) {
+    return res.status(400).json({ message: "Username can't be empty" });
+  }
+  
+  if (!email) {
+    return res.status(400).json({ message: "Email can't be empty" });
+  }
+  
+  if (!password) {
+    return res.status(400).json({ message: "Password can't be empty" });
+  }
+
 
   if (user) {
     res.status(403).json({ message: "User already exist, please login to continue" });
   } else {
-    const obj = { username: username, password: password };
+    const obj = { username: username, email: email, password: password };
     const newUser = new User(obj);
     newUser.save();
-    const token = jwt.sign({ username, role: "user" }, "SECRET", {
+    const token = jwt.sign({ email, role: "user" }, "SECRET", {
       expiresIn: "3h",
     });
     res.json({ message: "User registered successfully", token });
