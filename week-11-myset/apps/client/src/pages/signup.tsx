09b83880f4ce9baffe1924d5e7aec3244aeb3051
GourabSanyal <sanyal.gourab@gmail.com>
2024-01-29 18:54:25 +0530

    ui - error handling in user sign up

--- week-11-myset/apps/client/src/pages/signup.tsx
+++ week-11-myset/apps/client/src/pages/signup.tsx
@@ -2,26 +2,44 @@ import { Signup } from "ui";
 import axios from "axios";
 import { userState } from "store";
 import { useRouter } from "next/navigation";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import { useSetRecoilState } from "recoil";
 
 export default function SignPage() {
   const setUser = useSetRecoilState(userState)
   const router = useRouter();
+  const [onError, setOnError] = useState<string | null | undefined>(null);
+
+  const signUp = async (username: string, email: string, password: string) => {
+    try {
+      const response = await axios.post("/api/auth/user/signup", {
+        username,
+        email,
+        password,
+      });
+      // localStorage.setItem("token", response.data.token);
+      // setUser({ isLoading: false, userEmail: email });
+      // router.push("/courses");
+      console.log('res --> ',response);
+      
+    } catch (error) {
+      if (axios.isAxiosError(error)){
+          let errorRes
+        if (error.response?.status === 403) {
+          errorRes = error.response.data.message
+        } else {
+          errorRes = error.message || "An error occured"
+        }
+        setOnError(errorRes)
+      }
+    }
+  }
 
   return (
     <div>
       <Signup
-        onClick={async (username, email, password) => {
-          const response = await axios.post("/api/auth/signup", {
-            username,
-            email,
-            password,
-          });
-          localStorage.setItem("token", response.data.token);
-          setUser({isLoading: false, userEmail: email})
-          router.push("/courses");        
-        }}
+        onClick={signUp}
+        onError={onError}
       />
     </div>
   );
