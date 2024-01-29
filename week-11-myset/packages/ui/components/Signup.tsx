4cca2a882068dac61da4719d31b300e65c011852
GourabSanyal <sanyal.gourab@gmail.com>
2024-01-29 18:54:41 +0530

    ui - sign up component updated for user sign up

--- week-11-myset/packages/ui/components/Signup.tsx
+++ week-11-myset/packages/ui/components/Signup.tsx
@@ -5,14 +5,44 @@
 
 "use client";
 
-import { Typography, Card, TextField, Button } from "@mui/material";
+import {
+  Typography,
+  InputAdornment,
+  Card,
+  TextField,
+  Button,
+  IconButton,
+} from "@mui/material";
 import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
+import { Interface } from "readline";
 
-export function Signup(props: {
-  onClick: (username: string, password: string) => void;
-}) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+interface SignupProps {
+  onClick: (username: string, email: string, password: string) => void;
+  onError?: string | null | undefined;
+}
+interface FormData {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export function Signup({ onError, onClick }: SignupProps): JSX.Element {
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    onClick(data.username, data.email, data.password);
+  };
+
+  const handleTogglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <div>
@@ -24,44 +54,86 @@ export function Signup(props: {
           justifyContent: "center",
         }}
       >
-        <Typography variant={"h6"}>
-          Welcome to Coursera. Sign up below
-        </Typography>
+        <Typography variant="h6">Welcome to Coursera. Sign up below</Typography>
       </div>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <Card style={{ width: 400, padding: 20 }}>
-          <TextField
-            onChange={(event) => {
-              setEmail(event.target.value);
-            }}
-            fullWidth
-            label="Email"
-            variant="outlined"
-          />
-          <br />
-          <br />
-          <TextField
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-            fullWidth
-            label="Password"
-            variant="outlined"
-            type="password"
-          />
-          <br />
-          <br />
-
-          <Button
-            size={"large"}
-            variant="contained"
-            onClick={() => {
-              props.onClick(email, password);
-            }}
-          >
-            {" "}
-            Signup
-          </Button>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <TextField
+              label="Username"
+              fullWidth
+              margin="normal"
+              {...register("username", {
+                required: "Username is required",
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+$/,
+                  message: "Invalid username",
+                },
+              })}
+              error={!!errors.username}
+              helperText={errors.email?.message}
+            />
+            <TextField
+              label="Email"
+              fullWidth
+              margin="normal"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                  message: "Invalid email address",
+                },
+              })}
+              error={!!errors.email}
+              helperText={errors.email?.message}
+            />
+            <TextField
+              label="Password"
+              type={showPassword ? "text" : "password"}
+              fullWidth
+              margin="normal"
+              required
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 10,
+                  message: "Password must be at least 10 characters long",
+                },
+                pattern: {
+                  value: /\d+/gi,
+                  message: "Password must contain at least one number",
+                },
+              })}
+              error={!!errors.password}
+              helperText={errors.password?.message}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={handleTogglePasswordVisibility}
+                      edge="end"
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+            />
+            {onError ? (
+              <Typography
+                variant="body2"
+                align="center"
+                style={{ marginTop: 10, color: "red" }}
+              >
+                {onError}
+              </Typography>
+            ) : null}
+            <br />
+            <br />
+            <Button type="submit" size="large" variant="contained">
+              Signup
+            </Button>
+          </form>
         </Card>
       </div>
     </div>
