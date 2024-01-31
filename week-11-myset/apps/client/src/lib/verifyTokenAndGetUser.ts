70f9aae2036cfdf3c6de0427f33e55e701f8eb7a
GourabSanyal <sanyal.gourab@gmail.com>
2024-01-31 21:27:13 +0530

    feat - token params updated for cb funtion

--- week-11-myset/apps/client/src/lib/verifyTokenAndGetUser.ts
+++ week-11-myset/apps/client/src/lib/verifyTokenAndGetUser.ts
@@ -1,12 +1,10 @@
 import  jwt  from "jsonwebtoken";
 
-type CallbackFunction = (result : boolean | object) => void;
-
-export const verifyTokenAndGetUser = (token : string, cb: CallbackFunction) => {
+export const verifyTokenAndGetUser = (token : string, cb: any) => {
     jwt.verify(token, "SECRET", (err, user) => {
         if (err) {
             return cb(false)
         }
-        return cb(user as object)
+        return cb(user)
     })
 }
