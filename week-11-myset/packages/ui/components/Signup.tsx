e9698504842019efb64cdf493394fa5539ac9fbd
GourabSanyal <sanyal.gourab@gmail.com>
2024-02-07 22:15:32 +0530

    ui - signup user migration  to states from react form hook

--- week-11-myset/packages/ui/components/Signup.tsx
+++ week-11-myset/packages/ui/components/Signup.tsx
@@ -1,13 +1,4 @@
-/* eslint-disable @typescript-eslint/no-misused-promises */
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable react/button-has-type */
-/* eslint-disable react/jsx-sort-props */
-/* eslint-disable react/self-closing-comp */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-
-"use client";
-
+/* eslint-disable unicorn/filename-case */
 import {
   Typography,
   InputAdornment,
@@ -17,44 +8,16 @@ import {
   IconButton,
 } from "@mui/material";
 import { useState } from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
-import axios from "axios";
-import { useSetRecoilState } from "recoil";
-import { userState } from "store";
 
-interface SignupProps {
-  onSubmit: (username: string, email: string, password: string) => void;
-  onError?: string | null | undefined;
-}
-interface FormData {
-  username: string;
-  email: string;
-  password: string;
-}
-
-export function Signup({ onError, onSubmit }: SignupProps): JSX.Element {
+export function Signup(props: {
+  onClick: (username: string, email: string, password: string) => void;
+  onError: string | undefined;
+}): React.JSX.Element {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-  const [submissionError, setSubmissionError] = useState(null);
-  const {
-    register,
-    formState: { errors },
-  } = useForm<FormData>();
-
-  const [formData, setFormData] = useState<FormData>({
-    username: "",
-    email: "",
-    password: "",
-  });
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleFormSubmit = (data: FormData) => {
-    console.log("form submit --> ", data);
-  };
 
   const handleTogglePasswordVisibility = () => {
     setShowPassword(!showPassword);
@@ -70,81 +33,76 @@ export function Signup({ onError, onSubmit }: SignupProps): JSX.Element {
           justifyContent: "center",
         }}
       >
-        <Typography variant="h6">Welcome to Coursera. Sign up below</Typography>
+        <Typography variant="h6">Welcome to Coursera. Sign In below</Typography>
       </div>
       <div style={{ display: "flex", justifyContent: "center" }}>
         <Card style={{ width: 400, padding: 20 }}>
-          <form>
-            <TextField
-              label="Username"
-              name="username"
-              value={formData.username}
-              onChange={handleChange}
-              fullWidth
-              margin="normal"
-            />
-            <br />
-            <br />
-            <TextField
-              onChange={(event) => {
-                // setEmail(event.target.value);
-              }}
-              fullWidth
-              label="Email"
-              variant="outlined"
-            />
-            <br />
-            <br />
-            <TextField
-              onChange={(e) => {
-                // setPassword(e.target.value);
-              }}
-              fullWidth
-              label="Password"
-              variant="outlined"
-              type={showPassword ? "text" : "password"}
-              // margin="normal"
-              // {...register("password", {
-              //   required: "Password is required",
-              //   minLength: {
-              //     value: 10,
-              //     message: "Password must be at least 10 characters long",
-              //   },
-              //   pattern: {
-              //     value: /\d+/gi,
-              //     message: "Password must contain at least one number",
-              //   },
-              // })}
-              // error={!!errors.password}
-              helperText={errors.password?.message}
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={handleTogglePasswordVisibility}
-                      edge="end"
-                    >
-                      {showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <br />
-            <br />
-            <Button type="submit" size="large" variant="contained">
-              Signup
-            </Button>
-            {onError ? (
-              <Typography
-                variant="body2"
-                align="center"
-                style={{ color: "red" }}
-              >
-                {submissionError}
-              </Typography>
-            ) : null}
-          </form>
+          <TextField
+            fullWidth
+            label="Username"
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+            type="username"
+            variant="outlined"
+          />
+          <br />
+          <br />
+          <TextField
+            fullWidth
+            label="Email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="email"
+            variant="outlined"
+          />
+          <br />
+          <br />
+          <TextField
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    edge="end"
+                    onClick={handleTogglePasswordVisibility}
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+            fullWidth
+            label="Password"
+            onChange={(event) => {
+              setPassword(event.target.value);
+            }}
+            type={showPassword ? "text" : "password"}
+            variant="outlined"
+          />
+          <br />
+          <br />
+          {props.onError ? (
+            <Typography
+              align="center"
+              style={{ marginTop: 10, color: "red" }}
+              variant="body2"
+            >
+              {props.onError}
+            </Typography>
+          ) : null}
+          <br />
+          <br />
+          <Button
+            onClick={() => {
+              props.onClick(email, password, username);
+            }}
+            size="large"
+            variant="contained"
+          >
+            {" "}
+            Sign Up
+          </Button>
         </Card>
       </div>
     </div>
