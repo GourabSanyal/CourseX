1f85e017577cf507b1a92bd2e7a7615848d4c92c
GourabSanyal <sanyal.gourab@gmail.com>
2024-03-17 18:29:05 +0530

    state - exports fixed and indexed

--- packages/store/src/index.ts
+++ packages/store/src/index.ts
@@ -1,5 +1,15 @@
+// course
 export * from "./atoms/course"
-export * from "./atoms/user"
 export * from "./selectors/course"
+
+//user 
+export * from "./atoms/user"
 export * from "./selectors/userEmail"
 export * from "./selectors/isUserLoading"
+export * from "./selectors/userName"
+
+// admin
+export * from "./atoms/admin/admin"
+export * from "./selectors/admin/adminEmail"
+export * from "./selectors/admin/isAdminLoading"
+
