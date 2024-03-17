fa784da43fe44e8f6ff98246e0a19663c11ed95a
GourabSanyal <sanyal.gourab@gmail.com>
2024-03-17 18:11:46 +0530

    state - admin atom added

---
+++ packages/store/src/atoms/admin/admin.ts
@@ -0,0 +1,10 @@
+import {atom} from "recoil";
+import { recoilPersist } from 'recoil-persist';
+
+export const adminState = atom({
+  key: 'adminState',
+  default: {
+    isLoading: true,
+    userEmail: null as string | null,
+  },
+});
