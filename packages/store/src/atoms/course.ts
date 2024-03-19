ea628a74673e83372bc08ba5f6cfb3778e8f7f25
GourabSanyal <sanyal.gourab@gmail.com>
2024-03-19 12:15:13 +0530

    store - course state selector key name update

--- packages/store/src/atoms/course.ts
+++ packages/store/src/atoms/course.ts
@@ -1,7 +1,7 @@
 import {atom} from "recoil";
 
 export const courseState = atom<{isLoading: boolean, course: any}>({
-  key: 'courseState',
+  key: 'courseStateSelector',
   default: {
     isLoading: true,
     course: null
