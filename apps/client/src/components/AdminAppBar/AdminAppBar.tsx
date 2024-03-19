0efcc19d4f050ef2ec83aa882630bd07270173b3
GourabSanyal <sanyal.gourab@gmail.com>
2024-03-19 21:51:58 +0530

    ui - admin app bar component ui updated

--- apps/client/src/components/AdminAppBar/AdminAppBar.tsx
+++ apps/client/src/components/AdminAppBar/AdminAppBar.tsx
@@ -1,75 +1,195 @@
-import React from "react";
-import { Button, Typography } from "@mui/material";
-import { ToastContainer, toast } from "react-toastify";
-import { useRouter } from "next/navigation";
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import React, { useState } from "react";
+// import { Button, Typography, AppBar, Toolbar, useTheme, useMediaQuery } from "@mui/material";
 import {
-  adminUserName,
-  adminState,
-  isAdminLoading,
-  isUserLoading,
-  userEmailState,
-  userState,
-} from "store";
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+  Button,
+  useMediaQuery,
+  useTheme,
+} from "@material-ui/core";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useRouter } from "next/navigation";
+import { adminState } from "store";
+import { useSetRecoilState } from "recoil";
 
 function AdminAppBar() {
   const router = useRouter();
-  const adminLoading = useRecoilValue(isAdminLoading);
   const setAdmin = useSetRecoilState(adminState);
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm")); // Adjust the breakpoint as needed
+
+  const handleMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    console.log("clicked");
+    setAdmin({ isLoading: false, userEmail: null, username: null });
+    router.push("/");
+  };
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        paddingTop: 4,
-      }}
-    >
-          <div style={{ cursor: "pointer" }} onClick={() => router.push("/")}>
-            <Typography variant={"h6"}>Coursera</Typography>
-          </div>
-          <div style={{ display: "flex" }}>
-            <div style={{ marginRight: 10 }}>
-              {/* <Button
-                // variant={"contained"}
-                onClick={() => {
-                    openAdminLoginModal();
-                }}
-                >
-                Admin Login
-            </Button> */}
-              {/* <AdminModal
-                open={modalOpen}
-                onClose={handleCloseModal}
-                onSubmit={handleAdminSubmission}
-                isSignInRef={isSignInRef}
-                onError={onError}
-                handleResetError={handleResetError}
-            /> */}
-            </div>
-            <div style={{ marginRight: 10 }}>
-              <Button
-                variant={"contained"}
-                onClick={() => {
-                  router.push("/user/signin");
-                }}
-              >
-                Admin appbar
-              </Button>
-            </div>
+    <AppBar>
+      <Toolbar>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            flexGrow: 1,
+          }}
+        >
+          <Typography variant={"h5"}>CourseX</Typography>
+          {isMobile ? (
             <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                  keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem
+                  onClick={() => {
+                    router.push("/");
+                    handleClose();
+                  }}
+                >
+                  Your Courses
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    router.push("/");
+                    handleClose();
+                  }}
+                >
+                  Add Course
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    router.push("/");
+                    handleClose();
+                  }}
+                >
+                  All Course
+                </MenuItem>
+                <MenuItem onClick={handleLogout}>Logout</MenuItem>
+              </Menu>
+            </div>
+          ) : (
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                paddingTop: 4,
+              }}
+            >
               <Button
-                variant={"contained"}
+              style={{ color : 'white'}}
                 onClick={() => {
-                  router.push("/user/signup");
+                  router.push("/");
                 }}
               >
-                Sign Up
+                Your Courses
+              </Button>
+              <Button
+                style={{ color : 'white'}}
+                onClick={() => {
+                  router.push("/");
+                }}
+              >
+                Add Course
+              </Button>
+              <Button
+              style={{ color : 'white'}}
+                onClick={() => {
+                  router.push("/");
+                }}
+              >
+                All Course
+              </Button>
+              <Button variant="contained" onClick={handleLogout}>
+                Logout
               </Button>
             </div>
+          )}
+          {/* <div style={{ display: "flex" }}> */}
+          {/* <div style={{ marginRight: 10 }}></div> */}
+          {/* <div style={{ marginRight: 10 }}>
+            <Button
+              onClick={() => {
+                console.log("clicked");
+                router.push("/");
+              }}
+            >
+              Your Courses
+            </Button>
+            <Button
+              onClick={() => {
+                console.log("clicked");
+                router.push("/");
+              }}
+            >
+              Add Course
+            </Button>
+            <Button
+              onClick={() => {
+                console.log("clicked");
+                router.push("/");
+              }}
+            >
+              All Course
+            </Button>
           </div>
-        {/* </> */}
-    {/* //   ) : null} */}
-    </div>
+          <div>
+            <Button
+              variant={"contained"}
+              onClick={() => {
+                console.log("clicked");
+                setAdmin({
+                  isLoading: false,
+                  userEmail: null,
+                  username: null,
+                });
+                router.push("/");
+              }}
+            >
+              Logout
+            </Button>
+          </div>
+        </div> */}
+        </div>
+      </Toolbar>
+    </AppBar>
+    // </div>
   );
 }
 
