a80600049021587d8ae760a537634272ab73b2fd
GourabSanyal <sanyal.gourab@gmail.com>
2024-03-19 12:16:09 +0530

    store - user state default value type updated

--- packages/store/src/atoms/user.ts
+++ packages/store/src/atoms/user.ts
@@ -11,8 +11,8 @@ export const userState = atom({
   key: "userState",
   default: {
     isLoading: true,
-    userEmail: null as string | null,
-    username: null as string | null,
+    userEmail: null as string | null | undefined,
+    username: null as string | null | undefined,
   },
   // effects_UNSTABLE: [persistAtom]
 });
