35459c364d3f0b16a64f173ce9a26a69c1b2567d
GourabSanyal <sanyal.gourab@gmail.com>
2024-03-22 21:59:24 +0530

    route - dahsboard route name update

---
+++ apps/client/src/pages/admin/dashboard.tsx
@@ -0,0 +1,30 @@
+import { Box, Grid, Tab, Typography } from "@mui/material";
+import { Tabs } from "@mui/material";
+import React from "react";
+import { useRecoilValue } from "recoil";
+import { adminState } from "store";
+
+const dashboard = () => {
+  const adminUsername = useRecoilValue(adminState).username;
+  return (
+    <div
+      style={{ display: "flex", justifyContent: "center", minHeight: "100vh" }}
+    >
+      <div style={{ marginTop: "9vh" }}>
+        <Grid xl={2}>
+          <Typography variant="h4">
+            Welcome {adminUsername}
+          </Typography>
+          <div style={{ justifyContent: "center", alignItems: 'center' }}>
+            <Tabs style={{ marginLeft: "-20px"}}>
+              <Tab label="Your courses" />
+              <Tab label="All courses" />
+            </Tabs>
+          </div>
+        </Grid>
+      </div>
+    </div>
+  );
+};
+
+export default dashboard;
