02e8d7c58b3ff9aa1533f5aab1b6377893a71bac
GourabSanyal <sanyal.gourab@gmail.com>
2024-04-20 18:46:33 +0530

    api - verifytoken user type changed for jwtpayload

--- apps/client/src/lib/verifyTokenAndGetUser.ts
+++ apps/client/src/lib/verifyTokenAndGetUser.ts
@@ -1,10 +1,10 @@
-import  jwt  from "jsonwebtoken";
+import  jwt, { JwtPayload }  from "jsonwebtoken";
 
-export const verifyTokenAndGetUser = (token : string, cb: any) => {
+export const verifyTokenAndGetUser = (token : string, cb:(user: JwtPayload | boolean) => void) => {
     jwt.verify(token, "SECRET", (err, user) => {
         if (err) {
             return cb(false)
         }
-        return cb(user)
+        return cb(user as JwtPayload)
     })
 }
