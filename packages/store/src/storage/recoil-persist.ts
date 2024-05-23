af88f7ebbd94c2f047b4697c7e395590d084763c
GourabSanyal <sanyal.gourab@gmail.com>
2024-05-23 16:39:34 +0530

    recoil persist added for testing

--- packages/store/src/storage/recoil-persist.ts
+++ packages/store/src/storage/recoil-persist.ts
@@ -1,29 +1,29 @@
-import { recoilPersist} from 'recoil-persist'
+// import { recoilPersist} from 'recoil-persist'
 
-export const getStorage = (key: string) => {
-    if (typeof window !== 'undefined') {
-      return window.localStorage.getItem(key);
-    }
-    return null;
-  };
-  
-  export const setStorage = (key: string, value: string) => {
-    if (typeof window !== 'undefined') {
-      window.localStorage.setItem(key, value);
-    }
-  };
-  
-//   export const removeStorage = (key: string) => {
+// export const getStorage = (key: string) => {
 //     if (typeof window !== 'undefined') {
-//       window.localStorage.removeItem(key);
+//       return window.localStorage.getItem(key);
+//     }
+//     return null;
+//   };
+  
+//   export const setStorage = (key: string, value: string) => {
+//     if (typeof window !== 'undefined') {
+//       window.localStorage.setItem(key, value);
 //     }
 //   };
+  
+// //   export const removeStorage = (key: string) => {
+// //     if (typeof window !== 'undefined') {
+// //       window.localStorage.removeItem(key);
+// //     }
+// //   };
 
-  export const { persistAtom } = recoilPersist({
-    key: 'recoil-persist',
-    storage: {
-      getItem: (key : string) => getStorage(key),
-      setItem: (key : string, value : string) => setStorage(key, value),
-    //   removeItem: (key : string) => removeStorage(key),
-    },
-  });
+//   export const { persistAtom } = recoilPersist({
+//     key: 'recoil-persist',
+//     storage: {
+//       getItem: (key : string) => getStorage(key),
+//       setItem: (key : string, value : string) => setStorage(key, value),
+//     //   removeItem: (key : string) => removeStorage(key),
+//     },
+//   });
