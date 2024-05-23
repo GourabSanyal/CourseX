15e885c733b50d68eeefcf12cf97a5a9a23aae09
GourabSanyal <sanyal.gourab@gmail.com>
2024-05-23 16:35:19 +0530

    api - user purchased courses api added

---
+++ apps/client/src/pages/api/user/purchased-courses.tsx
@@ -0,0 +1,65 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { ensureDbConnected } from "@/lib/dbConnect";
+import { verifyTokenAndGetUser } from "@/lib/verifyTokenAndGetUser";
+import { Admin, Course, User } from "db";
+import { JwtPayload } from "jsonwebtoken";
+
+type Course = {
+  _id: string;
+  description: string;
+  price: number;
+  imageLink: string;
+  published: boolean;
+};
+
+type ErrorObj = {
+  message: string;
+  statusCode: number;
+};
+
+type ResponseData =
+  | {
+      message: string;
+      data: Course[] | null;
+    }
+  | ErrorObj;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  try {
+    await ensureDbConnected();
+    const authHeader = req.headers.authorization;
+    if (authHeader) {
+      const token = authHeader.split(" ")[1];
+      verifyTokenAndGetUser(token, async (user: JwtPayload | boolean) => {
+        let email = (user as JwtPayload).email;
+        let loggeduser = await User.findOne({ email }).populate(
+          "purchasedCourses"
+        );
+
+        const courses  = loggeduser.purchasedCourses;
+
+        if (!courses.length) {
+          res.json({
+            message: "You have not bought any course yet!",
+            statusCode: 200,
+          });
+        } else {
+          res.json({ message: "This is all your courses", data: courses });
+        }
+      });
+    } else {
+      res.status(400).json({
+        message: "No auth token available, login to continue",
+        statusCode: 400,
+      });
+    }
+  } catch (error) {
+    res.json({
+      message: "Error from api admin/your-coourses",
+      statusCode: 401,
+    });
+  }
+}
