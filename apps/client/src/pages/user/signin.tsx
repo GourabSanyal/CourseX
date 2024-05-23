79d98870c38ed2f1d7ff9ee5ec5d936efea314dc
GourabSanyal <sanyal.gourab@gmail.com>
2024-05-23 16:37:07 +0530

    ui - user sign in logic in up updated

--- apps/client/src/pages/user/signin.tsx
+++ apps/client/src/pages/user/signin.tsx
@@ -4,13 +4,13 @@ import { userState } from "store";
 import { useRouter } from "next/navigation";
 import { useSetRecoilState } from "recoil";
 import { useState } from "react";
-import { useSession } from 'next-auth/react'
+import { useSession } from "next-auth/react";
 
 export default function UserSignInPage() {
   const [onError, setOnError] = useState("");
-  const setUser = useSetRecoilState(userState)
+  const setUser = useSetRecoilState(userState);
   const router = useRouter();
-  // const {data: session, status} = useSession(); 
+  // const {data: session, status} = useSession();
 
   const handleSubmit = async (email: string, password: string) => {
     try {
@@ -19,9 +19,9 @@ export default function UserSignInPage() {
         password,
       });
       localStorage.setItem("token", response.data.token);
-      document.cookie = `token=${response.data.token}`
       router.push("/user/home");
-      setUser({ isLoading: false, userEmail: email, username: null})            
+      let username = response.data.username;
+      setUser({ isLoading: false, userEmail: email, username: username });
     } catch (error) {
       if (axios.isAxiosError(error) && error.response?.status === 403) {
         let errorRes = error.response.data.message;
@@ -30,14 +30,11 @@ export default function UserSignInPage() {
         console.log("An error occurred: ", error);
       }
     }
-  }
+  };
 
   return (
     <div>
-      <Signin
-        onClick={handleSubmit}
-        onError={onError}
-      />
+      <Signin onClick={handleSubmit} onError={onError} />
     </div>
   );
 }
