82d25c4ea008221b87c55a9fc8ad67d50fa7ff95
GourabSanyal <sanyal.gourab@gmail.com>
2024-05-23 17:10:24 +0530

    db - db connection logic updated to handle db url error

--- apps/client/src/lib/dbConnect.ts
+++ apps/client/src/lib/dbConnect.ts
@@ -3,10 +3,14 @@ import mongoose from "mongoose"
 let alreadyDone = false
 
 export async function ensureDbConnected(){
+    const dbUrl = process.env.DB_CONNECTION_URL
+    if (!dbUrl){
+        console.log("Database connection URL is not defined");
+    }
     if(alreadyDone){
         return;
     }
     alreadyDone = true 
-    await mongoose.connect("");
+    await mongoose.connect(dbUrl as string);
     console.log("db connected");
 }
