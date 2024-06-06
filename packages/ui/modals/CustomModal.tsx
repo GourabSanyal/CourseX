2203beb1497981bf52dbfd46ab1fcd5c9b7c30d0
Gourab Sanyal <sanyal.gourab@gmail.com>
2024-06-06 23:08:08 +0530

    custom modal init

--- packages/ui/modals/CustomModal.tsx
+++ packages/ui/modals/CustomModal.tsx
@@ -1,108 +1,84 @@
-/* eslint-disable react/function-component-definition */
-/* eslint-disable react/jsx-sort-props */
-/* eslint-disable react/jsx-no-leaked-render */
-/* eslint-disable @typescript-eslint/explicit-function-return-type */
-/* eslint-disable react/button-has-type */
+/* eslint-disable no-implicit-coercion */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-misused-promises */
+/* eslint-disable @typescript-eslint/await-thenable */
 /* eslint-disable @typescript-eslint/no-confusing-void-expression */
-/* eslint-disable no-alert */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable react/jsx-sort-props */
+/* eslint-disable import/no-extraneous-dependencies */
 
-import React, { ReactNode } from 'react';
+import React from "react";
 import {
-  Modal,
-  Box,
+  Dialog,
+  DialogContent,
+  DialogTitle,
   Typography,
   Button,
-  Fade,
-  styled,
-} from '@mui/material';
-
-// Styled components for customization
-const ModalContainer = styled(Box)`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background-color: #fff;
-  border-radius: 8px;
-  padding: 24px;
-  outline: none;
-`;
-
-const ModalHeader = styled(Box)`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 16px;
-`;
-
-const ModalContent = styled(Box)`
-  margin-bottom: 24px;
-`;
-
-const ModalActions = styled(Box)`
-  display: flex;
-  justify-content: flex-end;
-  gap: 16px;
-`;
+  Box,
+} from "@mui/material";
 
 interface CustomModalProps {
   open: boolean;
-  onClose: () => void;
-  title?: string;
-  subTitle?: string;
-  primaryButtonText?: string;
-  primaryButtonAction?: () => void;
-  secondaryButtonText?: string;
-  secondaryButtonAction?: () => void;
+  onClose?: () => void;
+  onError?: string | null | undefined;
+  heading: string;
+  subHeading: string;
 }
 
-export const CustomModal : React.FC <CustomModalProps> = ({
+export function CustomModal({
   open,
   onClose,
-  title,
-  subTitle,
-  primaryButtonText,
-  primaryButtonAction,
-  secondaryButtonText,
-  secondaryButtonAction,
-}) => {
+  onError,
+  heading,
+  subHeading,
+}: CustomModalProps): JSX.Element {
   return (
-    <Modal
-      open={open}
-      onClose={onClose}
-      closeAfterTransition
-    >
-      <Fade in={open}>
-        <ModalContainer>
-          <ModalHeader>
-            <Typography variant="h6">{title}</Typography>
-            <Button onClick={onClose}>Close</Button>
-          </ModalHeader>
-          <ModalContent>
-            <Typography variant="body1">{subTitle}</Typography>
-          </ModalContent>
-          <ModalActions>
-            {primaryButtonText && (
-              <Button
-                variant="contained"
-                onClick={primaryButtonAction}
-                color="primary"
-              >
-                {primaryButtonText}
-              </Button>
-            )}
-            {secondaryButtonText && (
-              <Button
-                variant="outlined"
-                onClick={secondaryButtonAction}
-                color="secondary"
-              >
-                {secondaryButtonText}
-              </Button>
-            )}
-          </ModalActions>
-        </ModalContainer>
-      </Fade>
-    </Modal>
+    <Dialog open={open} onClose={onClose} maxWidth="sm" fullWidth>
+      <DialogTitle>
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Box>
+            <Typography variant="h6" component="h2">
+              {heading}
+            </Typography>
+            <Typography variant="subtitle1" component="h3" gutterBottom>
+              {subHeading}
+            </Typography>
+          </Box>
+          <Button onClick={onClose} style={{ marginLeft: 'auto' }}>
+            X
+          </Button>
+        </Box>
+      </DialogTitle>
+      <DialogContent dividers>
+        {onError ? (
+          <Typography
+            variant="body2"
+            align="center"
+            style={{ marginTop: 10, color: "red" }}
+          >
+            {onError}
+          </Typography>
+        ) : null}
+        {/* Other content can go here */}
+      </DialogContent>
+    </Dialog>
   );
-};
+}
+
+
+
+
+
+// {/* <Typography variant="body2" align="center" style={{ marginTop: 10 }}>
+//           {isSignInRef.current
+//             ? "Don't have an admin account? "
+//             : "Already have an admin account? "}
+//           <Link component="button" variant="body2" onClick={handleToggleForm}>
+//             {isSignInRef.current ? "Register as an Admin here" : "Sign In here"}
+//           </Link>
+//         </Typography> */}
