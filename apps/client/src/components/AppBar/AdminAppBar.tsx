38b830b1194a7d93c52ed9de065b78cb8c2a50e3
Gourab Sanyal <sanyal.gourab@gmail.com>
2024-06-21 23:27:22 +0530

    ui - admin app bar redesign + component creation

---
+++ apps/client/src/components/AppBar/AdminAppBar.tsx
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  useMediaQuery,
+  useTheme,
+} from "@material-ui/core";
+import { signOut } from "next-auth/react";
+import { useRouter } from "next/router";
+import MenuIcon from "@mui/icons-material/Menu"; // Ensure you have this import
+
+const AdminAppBar = () => {
+  // Responsiveness mobile logic
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm")); // Adjust the breakpoint as needed
+  const router = useRouter();
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const adminLogout = () => {
+    console.log("admin logout");
+    signOut();
+  };
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            flexGrow: 1,
+          }}
+        >
+          <div onClick={() => router.push("/")}>
+            <Typography variant={"h6"}>Coursera</Typography>
+          </div>
+          {isMobile ? (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem
+                  onClick={() => {
+                    handleClose();
+                    adminLogout();
+                  }}
+                >
+                  Dashboard
+                </MenuItem>
+                <MenuItem
+                  onClick={() => {
+                    handleClose();
+                    router.push("/user/signin");
+                  }}
+                >
+                  Add Course
+                </MenuItem>
+                <MenuItem onClick={adminLogout}>Log Out</MenuItem>
+              </Menu>
+            </div>
+          ) : (
+            <div style={{ display: "flex", marginLeft: "auto" }}>
+              <Button
+                style={{ color: "white", marginRight: 10 }}
+                onClick={() => {
+                  // redirect to dashboard here
+                }}
+              >
+                Dashboard
+              </Button>
+              <Button
+                style={{ color: "white", marginRight: 10 }}
+                onClick={() => {
+                  // redirect to add course here
+                }}
+              >
+                Add Course
+              </Button>
+              <Button variant="contained" onClick={adminLogout}>
+                Logout
+              </Button>
+            </div>
+          )}
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default AdminAppBar;
