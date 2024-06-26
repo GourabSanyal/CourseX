e9b12e6c7ad3672bf241bdd7c01762711ba6f4b3
Gourab Sanyal <sanyal.gourab@gmail.com>
2024-06-26 16:54:21 +0530

    route - _app logic updated to handl;e logins from jwt to session

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -13,102 +13,141 @@ import { useEffect, useState } from "react";
 import Appbar from "@/components/AppBar";
 import AdminAppBar from "@/components/AdminAppBar/AdminAppBar";
 import UserAppBar from "@/components/UserAppBar/UserAppBar";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, useSession } from "next-auth/react";
+import { Session } from "next-auth";
 
-export default function App({ Component, pageProps, router }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+  router
+}: AppProps<{
+  session: Session | null;
+}>) {
   return (
-    <SessionProvider>
+    <SessionProvider session={pageProps.session}>
       <RecoilRoot>
-        <App2 Component={Component} pageProps={pageProps} router={router} />
+        <AppWrapper Component={Component} pageProps={pageProps} router={router} />
       </RecoilRoot>
     </SessionProvider>
   );
 }
 
-function App2({ Component, pageProps }: AppProps) {
-  const { router, ...restPageProps } = pageProps;
+function AppWrapper({ 
+  Component, 
+  pageProps,
+  router
+}: AppProps) {
+  const {data: session, status} = useSession()
 
-  const userLoading = useRecoilValue(isUserLoading);
-  const adminLoading = useRecoilValue(isAdminLoading);
-
-  const isAdmin = useRecoilValue(adminState).username;
-  const isUser = useRecoilValue(userState).username;
-
-  if (userLoading && adminLoading) {
-    return (
-      <div>
-        Loading...
-        <InitUser />
+  if (status === "loading"){
+    return(
+      <div >
+        Loading . . .
       </div>
-    );
+    )
   }
   return (
     <div>
-      {isUser ? <UserAppBar /> : isAdmin ? <AdminAppBar /> : <Appbar />}
-      <Component {...restPageProps} />
+      <Appbar />
+      <Component {...pageProps} />
     </div>
   );
 }
 
-function InitUser() {
-  const setUser = useSetRecoilState(userState);
-  const setAdmin = useSetRecoilState(adminState);
-  const init = async () => {
-    try {
-      const response = await axios.get(`/api/auth/me`, {
-        headers: {
-          Authorization: "Bearer " + localStorage.getItem("token"),
-        },
-      });
-      if (!response.data) {
-        setUser({
-          isLoading: false,
-          userEmail: response.data.email,
-          username: response.data.username,
-        });
-        setAdmin({
-          isLoading: false,
-          userEmail: response.data.email,
-          username: response.data.username,
-        });
-      } else if (response.data.role === "user") {
-        setAdmin({
-          isLoading: false,
-          userEmail: "",
-          username: "",
-        });
-        setUser({
-          isLoading: false,
-          userEmail: response.data.email,
-          username: response.data.username,
-        });
-      } else if (response.data.role === "admin") {
-        setUser({
-          isLoading: false,
-          userEmail: "",
-          username: "",
-        });
-        setAdmin({
-          isLoading: false,
-          userEmail: response.data.email,
-          username: response.data.username,
-        });
-      }
-    } catch (error) {
-      setUser({
-        isLoading: false,
-        userEmail: null,
-        username: null,
-      });
-      setAdmin({
-        isLoading: false,
-        userEmail: null,
-        username: null,
-      });
-    }
-  };
-  useEffect(() => {
-    init();
-  }, []);
-  return <></>;
-}
+// export default function App({ Component, pageProps, router }: AppProps) {
+//   return (
+//     <SessionProvider>
+//       <RecoilRoot>
+//         <App2 Component={Component} pageProps={pageProps} router={router} />
+//       </RecoilRoot>
+//     </SessionProvider>
+//   );
+// }
+
+// function App2({ Component, pageProps }: AppProps) {
+//   const { router, ...restPageProps } = pageProps;
+
+//   const userLoading = useRecoilValue(isUserLoading);
+//   const adminLoading = useRecoilValue(isAdminLoading);
+
+//   const isAdmin = useRecoilValue(adminState).username;
+//   const isUser = useRecoilValue(userState).username;
+
+//   if (userLoading && adminLoading) {
+//     return (
+//       <div>
+//         Loading...
+//         <InitUser />
+//       </div>
+//     );
+//   }
+//   return (
+//     <div>
+//       {isUser ? <UserAppBar /> : isAdmin ? <AdminAppBar /> : <Appbar />}
+//       <Component {...restPageProps} />
+//     </div>
+//   );
+// }
+
+// function InitUser() {
+//   const setUser = useSetRecoilState(userState);
+//   const setAdmin = useSetRecoilState(adminState);
+//   const init = async () => {
+//     try {
+//       const response = await axios.get(`/api/auth/me`, {
+//         headers: {
+//           Authorization: "Bearer " + localStorage.getItem("token"),
+//         },
+//       });
+//       if (!response.data) {
+//         setUser({
+//           isLoading: false,
+//           userEmail: response.data.email,
+//           username: response.data.username,
+//         });
+//         setAdmin({
+//           isLoading: false,
+//           userEmail: response.data.email,
+//           username: response.data.username,
+//         });
+//       } else if (response.data.role === "user") {
+//         setAdmin({
+//           isLoading: false,
+//           userEmail: "",
+//           username: "",
+//         });
+//         setUser({
+//           isLoading: false,
+//           userEmail: response.data.email,
+//           username: response.data.username,
+//         });
+//       } else if (response.data.role === "admin") {
+//         setUser({
+//           isLoading: false,
+//           userEmail: "",
+//           username: "",
+//         });
+//         setAdmin({
+//           isLoading: false,
+//           userEmail: response.data.email,
+//           username: response.data.username,
+//         });
+//       }
+//     } catch (error) {
+//       setUser({
+//         isLoading: false,
+//         userEmail: null,
+//         username: null,
+//       });
+//       setAdmin({
+//         isLoading: false,
+//         userEmail: null,
+//         username: null,
+//       });
+//     }
+//   };
+//   useEffect(() => {
+//     init();
+//   }, []);
+//   return <></>;
+// }
