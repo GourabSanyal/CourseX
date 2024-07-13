e97c479c041b7fa2b609369d853a74f77644d44b
Gourab Sanyal <sanyal.gourab@gmail.com>
2024-07-14 02:50:33 +0530

    api - common all courses route fixed with server side session and tokens

--- apps/client/src/pages/api/common/all-courses.tsx
+++ apps/client/src/pages/api/common/all-courses.tsx
@@ -2,8 +2,10 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { ensureDbConnected } from "@/lib/dbConnect";
 import { verifyTokenAndGetUser } from "@/lib/verifyTokenAndGetUser";
-import { Course } from "db";
+import { Admin, Course } from "db";
 import { JwtPayload } from "jsonwebtoken";
+import { getSession } from "next-auth/react";
+import { getToken } from "next-auth/jwt";
 
 type Course = {
   _id: string; // You can adjust this type as needed
@@ -19,30 +21,61 @@ type ErrorObj = {
   statusCode: number;
 };
 
+type ResponseData =
+  | {
+      message: string;
+      data: Course[] | null;
+    }
+  | ErrorObj;
+
+const secret = process.env.NEXTAUTH_SECRET;
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Course[] | ErrorObj>
+  res: NextApiResponse<ResponseData>
 ) {
   try {
     await ensureDbConnected();
-    const authHeader = req.headers.authorization;
-    if (authHeader) {
-      const token = authHeader.split(" ")[1];
-      verifyTokenAndGetUser(token, async (user: JwtPayload | boolean) => {
-        if (!user) {
-          const errorResponse: ErrorObj = {
-            message: "Auth token expired",
-            statusCode: 403,
-          };
-          res.status(403).json(errorResponse);
-        } else {
-          const courses: Course[] = await Course.find({});
-          res.status(200).json(courses);
-        }
+
+    await ensureDbConnected();
+    const session = await getSession({ req });
+    const token = await getToken({ req, secret });
+    console.log("JSON Web Token", token);
+    if (!session && !token ) {
+      res.json({
+        message: "Session expired, please relogin to continue",
+        statusCode: 403,
       });
     } else {
-      res.status(400).json({ message: "error", statusCode: 401 });
+      let courses: Course[] = await Course.find({});
+      if (!courses.length) {
+        res.json({
+          message: "You have not created any course yet!",
+          statusCode: 200,
+        });
+      } else {
+        res.json({ message: "This is all courses", data: courses });
+      }
     }
+
+    // const authHeader = req.headers.authorization;
+    // if (authHeader) {
+    //   const token = authHeader.split(" ")[1];
+    //   verifyTokenAndGetUser(token, async (user: JwtPayload | boolean) => {
+    //     if (!user) {
+    //       const errorResponse: ErrorObj = {
+    //         message: "Auth token expired",
+    //         statusCode: 403,
+    //       };
+    //       res.status(403).json(errorResponse);
+    //     } else {
+    //       const courses: Course[] = await Course.find({});
+    //       res.status(200).json(courses);
+    //     }
+    //   });
+    // } else {
+    //   res.status(400).json({ message: "error", statusCode: 401 });
+    // }
   } catch (error) {
     console.log("error from api -> ", error);
 
