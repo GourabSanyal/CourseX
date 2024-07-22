6c1fbb1cb79d578264789ef7b587a434599e7d2a
Gourab Sanyal <sanyal.gourab@gmail.com>
2024-07-22 15:20:54 +0530

    store - user store code clean up

--- packages/store/src/atoms/user.ts
+++ packages/store/src/atoms/user.ts
@@ -1,12 +1,4 @@
 import { atom } from "recoil";
-// import { persistAtom } from "../storage/recoil-persist";
-
-// const { persistAtom } = recoilPersist({
-//   key: 'persist',
-//   storage: localStorage,
-//   converter: JSON
-// })
-
 export const userState = atom({
   key: "userState",
   default: {
@@ -14,5 +6,4 @@ export const userState = atom({
     userEmail: null as string | null | undefined,
     username: null as string | null | undefined,
   },
-  // effects_UNSTABLE: [persistAtom]
 });
