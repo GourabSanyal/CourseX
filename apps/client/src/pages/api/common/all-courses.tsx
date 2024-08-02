53e2c5f630e41ab664056efb044af90969ce0671
Gourab Sanyal <sanyal.gourab@gmail.com>
2024-08-03 00:05:04 +0530

    api - common route ger server sesson add

--- apps/client/src/pages/api/common/all-courses.tsx
+++ apps/client/src/pages/api/common/all-courses.tsx
@@ -6,6 +6,8 @@ import { Course } from "db";
 // import { JwtPayload } from "jsonwebtoken";
 import { getSession } from "next-auth/react";
 import { getToken } from "next-auth/jwt";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 
 type Course = {
   _id: string;
@@ -36,9 +38,7 @@ export default async function handler(
 ) {
   try {
     await ensureDbConnected();
-
-    await ensureDbConnected();
-    const session = await getSession({ req });
+    const session = await getServerSession(req , res, authOptions);
     const token = await getToken({ req, secret });
     // console.log("JSON Web Token", token);
     if (!session && !token ) {
