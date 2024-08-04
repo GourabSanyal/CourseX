7556db005993991b63b8b744c998bca7526a317d
Gourab Sanyal <sanyal.gourab@gmail.com>
2024-08-04 11:03:57 +0530

    ui - minor, redundant code clean up

--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -17,16 +17,6 @@ export default function Home() {
   const router = useRouter();
   const { data: session, status } = useSession();
 
-  // const router = useRouter();
-  // const userEmail = useRecoilValue(userEmailState);
-  // const userLoading = useRecoilValue(isUserLoading);
-  // const adminEmail = useRecoilValue(adminEmailState);
-  // const adminLoading = useRecoilValue(isAdminLoading);
-
-  // if (userLoading) {
-  //   return <></>;
-  // }
-
   return (
     <div
       style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
@@ -77,82 +67,3 @@ export default function Home() {
     </div>
   );
 }
-// <div
-//   style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-// >
-//   <Grid
-//     container
-//     style={{
-//       padding: "5vw",
-//       flexWrap: "wrap-reverse",
-//       justifyContent: "center",
-//     }}
-//   >
-//     <Grid
-//       item
-//       xs={12}
-//       md={6}
-//       lg={6}
-//       style={{
-//         display: "flex",
-//         justifyContent: "center",
-//         alignItems: "center",
-//       }}
-//     >
-//       <div style={{ textAlign: "center" }}>
-//         <Typography variant="h2">CourseX</Typography>
-//         <Typography variant="h5">Learn, earn and grow</Typography>
-//         {(!userLoading && !userEmail && !adminEmail && !adminLoading) && (
-//           <div
-//             style={{
-//               display: "flex",
-//               justifyContent: "center",
-//               marginTop: 20,
-//             }}
-//           >
-//             <div style={{ marginRight: 10 }}>
-//               <Button
-//                 color="primary"
-//                 size="large"
-//                 variant="contained"
-//                 onClick={() => router.push("/user/signup")}
-//               >
-//                 Signup
-//               </Button>
-//             </div>
-//             <div>
-//               <Button
-//                 color="primary"
-//                 size="large"
-//                 variant="contained"
-//                 onClick={() => router.push("/user/signin")}
-//               >
-//                 Signin
-//               </Button>
-//             </div>
-//           </div>
-//         )}
-//       </div>
-//     </Grid>
-//     <Grid
-//       item
-//       xs={12}
-//       md={6}
-//       lg={6}
-//       style={{
-//         display: "flex",
-//         justifyContent: "center",
-//         alignItems: "center",
-//         margin: "auto",
-//       }}
-//     >
-//       <img
-//         src="https://img.freepik.com/free-vector/empty-classroom-interior-with-chalkboard_1308-65378.jpg"
-//         alt="course-image"
-//         style={{ maxWidth: "100%", height: "auto" }}
-//       />
-//     </Grid>
-//   </Grid>
-// </div>
-//   );
-// }
