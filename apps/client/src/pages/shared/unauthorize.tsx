10434707d8f42fc5822f447119fcb97a3ac25f89
Gourab Sanyal <sanyal.gourab@gmail.com>
2024-08-05 16:01:46 +0530

    ui - shared page prettier format

--- apps/client/src/pages/shared/unauthorize.tsx
+++ apps/client/src/pages/shared/unauthorize.tsx
@@ -1,12 +1,19 @@
-import { Typography } from '@mui/material'
-import React from 'react'
+import { Typography } from "@mui/material";
+import React from "react";
 
 const unauthorize = () => {
   return (
-    <Typography style={{ display: 'flex', alignItems: "center", justifyContent:"center", marginTop: '50vh'}}>
+    <Typography
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: "50vh",
+      }}
+    >
       You are unauthorize to access this page, please login to continue
-    </Typography> 
-  )
-}
+    </Typography>
+  );
+};
 
-export default unauthorize
+export default unauthorize;
