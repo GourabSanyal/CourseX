fb431d4ecaa905a107e0bf04b6cf1507efc5ad12
Gourab Sanyal <sanyal.gourab@gmail.com>
2024-11-29 18:00:55 +0530

    feat - force debouce for reload and rereoute, delay time update

--- apps/client/src/hooks/useCart.ts
+++ apps/client/src/hooks/useCart.ts
@@ -3,13 +3,13 @@ import { useRecoilState } from "recoil";
 import axios from "axios";
 import { cartState } from "store";
 
-const DEBOUNCE_DELAY = 10;
+const DEBOUNCE_DELAY = 10000;
 
 export const useCart = () => {
   const [cart, setCart] = useRecoilState(cartState);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-  const timeoutIdRef = useRef<NodeJS.Timeout | null>(null)
+  const timeoutIdRef = useRef<NodeJS.Timeout | null>(null);
   const latestCartRef = useRef(cart);
 
   // update ref if `cart` changes
@@ -32,16 +32,38 @@ export const useCart = () => {
 
   // starts sync with server - debounce
   const startDebouncedSync = () => {
-    
     if (timeoutIdRef.current) {
       clearTimeout(timeoutIdRef.current);
     }
-    const newTimeoutId = setTimeout(() => {
-      console.log("debounce called");
-      
+    setTimeout(() => {
       syncWithServer();
     }, DEBOUNCE_DELAY);
   };
+
+  const forceSync = async () => {
+    try {
+      console.log("fore sync called");
+      await syncWithServer();
+    } catch (error) {
+      console.log("error running forceSync ", error);
+    }
+  };
+
+  // page unload and navigation debounce
+  useEffect(() => {
+    const handleBeforeUnload = async (e: BeforeUnloadEvent) => {
+      await forceSync();
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+      if (timeoutIdRef.current) {
+        clearTimeout(timeoutIdRef.current);
+      }
+    };
+  }, []);
 
   return {
     cart,
@@ -49,5 +71,6 @@ export const useCart = () => {
     error,
     startDebouncedSync,
     syncWithServer,
+    forceSync,
   };
 };
