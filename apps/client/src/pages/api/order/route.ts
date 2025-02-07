07080eccca48dce3031b7093c29ef7d68011262d
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-02-07 23:01:30 +0530

    feat - apyment api intigration

--- apps/client/src/pages/api/order/route.ts
+++ apps/client/src/pages/api/order/route.ts
@@ -30,42 +30,37 @@ type ResponseData =
 const secret = process.env.NEXTAUTH_SECRET;
 
 const razorpay = new Razorpay({
-    key_id: process.env.key_id!,
-    key_secret: process.env.key_secret,
-   });
+  key_id: process.env.RAZORPAY_KEY_ID ?? '',
+  key_secret: process.env.RAZORPAY_KEY_SECRET ?? '',
+});
 
 export default async function POST(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
   try {
-    await ensureDbConnected();
-    const session = await getServerSession(req, res, authOptions);
-    const token = await getToken({ req, secret });
-    console.log("api error -> ", req.body);
-    
-    
-    if (!session && !token) {
-        // res.json({
-        //   message: "Session expired, please relogin to continue",
-        //   statusCode: 403,
-        // });
-    } else {
-        // const { amount, currency } = await req.body as {
-        //     amount : string,
-        //     currency : string
-        // };
+    // await ensureDbConnected();
+    // const session = await getServerSession(req, res, authOptions);
+    // const token = await getToken({ req, secret });
+    // console.log("api error -> ", req.body);
+    // console.log("in api", req.body)
+    const { amount, currency } = (await req.body) as {
+      amount: string;
+      currency: string;
+    };
 
-        // var options = {
-        //     amount: amount,
-        //     currency: currency,
-        //     receipt: 'rcp1',
-        //    };
-        //    const order = await razorpay.orders.create(options);
-        //    console.log(order);
-        //    return res.status(200).json({ orderID : order.id });
+    var options = {
+      amount: amount,
+      currency: currency,
+      receipt: "rcp1",
+    };
 
-    }
+    const order = await razorpay.orders.create(options);
+    console.log(order);
+
+    return res.status(200).json({
+        orderID : order.id,
+    })
   } catch (error) {
     res.json({
       message: "Error from order/route",
