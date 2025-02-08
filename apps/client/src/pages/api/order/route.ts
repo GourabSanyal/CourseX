f6d5d77c8886ae2540413bfc39c1ddc389bd52d5
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-02-08 22:20:53 +0530

    bug - payment api session handling, shared types add

--- apps/client/src/pages/api/order/route.ts
+++ apps/client/src/pages/api/order/route.ts
@@ -1,28 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { ensureDbConnected } from "@/lib/dbConnect";
-import { Admin, Course } from "db";
 import { getToken } from "next-auth/jwt";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]";
 import Razorpay from "razorpay";
-
-type Course = {
-  _id: string;
-  description: string;
-  price: number;
-  imageLink: string;
-  published: boolean;
-};
-
-type ErrorObj = {
-  message: string;
-  statusCode: number;
-};
+import { ErrorObj } from "shared-types";
 
 type ResponseData =
   | {
       message?: string;
-      data?: Course[] | null;
       orderID?: string;
       statusCode?: number;
     }
@@ -30,8 +15,8 @@ type ResponseData =
 const secret = process.env.NEXTAUTH_SECRET;
 
 const razorpay = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID ?? '',
-  key_secret: process.env.RAZORPAY_KEY_SECRET ?? '',
+  key_id: process.env.RAZORPAY_KEY_ID ?? "",
+  key_secret: process.env.RAZORPAY_KEY_SECRET ?? "",
 });
 
 export default async function POST(
@@ -43,28 +28,35 @@ export default async function POST(
     const token = await getToken({ req, secret });
     console.log("api error -> ", req.body);
     try {
+      if (!session && !token) {
+        res.json({
+          message: "Session expired, please relogin to continue",
+          statusCode: 403,
+        });
+      } else {
         const { amount, currency } = (await req.body) as {
           amount: string;
           currency: string;
         };
-    
+
         var options = {
           amount: amount,
           currency: currency,
           receipt: "rcp1",
         };
-    
+
         const order = await razorpay.orders.create(options);
         console.log(order);
-    
+
         return res.status(200).json({
-            orderID : order.id,
-        })
+          orderID: order.id,
+        });
+      }
     } catch (error) {
-        res.json({
-            message : "failed to complete payment",
-            statusCode : 400
-        })
+      res.json({
+        message: "failed to complete payment",
+        statusCode: 400,
+      });
     }
   } catch (error) {
     res.json({
