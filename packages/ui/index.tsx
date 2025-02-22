aa77c90bdf3b9802b17e7df9e1caa2947a8b3c84
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-02-22 17:07:26 +0530

    pkg - shadcn init to repo

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,7 +1,9 @@
 export { Card } from "./card";
-export { Button } from "./Button";
+export { Button } from "./components/Button";
+export type { ButtonProps } from "./components/Button";
 export { AdminModal } from "./modals/AdminModal";
 export { NewCourseCard } from './components/NewCourseCard';
-export {CustomModal} from './modals/CustomModal';
+export { CustomModal } from './modals/CustomModal';
+export {CustomButton} from './components/CustomButton'
 export * from "./animations/slideUpVariant";
 export * from './components';
