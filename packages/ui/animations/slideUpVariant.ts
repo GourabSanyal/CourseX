c5076a786019516eba4bf06377ef2c7b5d2cc910
Gourab Sanyal <sanyal.gourab@gmail.com>
2025-02-22 15:46:29 +0530

    pkg - slide up animation add + export

---
+++ packages/ui/animations/slideUpVariant.ts
@@ -0,0 +1,6 @@
+
+
+export const slideUpVariant = {
+    hidden: { opacity: 0, y: 9 },
+    visible: { opacity: 1, y: 0 },
+  }
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,11 +1,7 @@
-// import { AdminModal} from './modals/AdminModal'
-
-// component exports
 export { Card } from "./card";
 export { Button } from "./Button";
 export { AdminModal } from "./modals/AdminModal";
-
 export { NewCourseCard } from './components/NewCourseCard';
 export {CustomModal} from './modals/CustomModal';
-
+export * from "./animations/slideUpVariant";
 export * from './components';
